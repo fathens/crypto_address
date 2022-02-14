691be31cbf079e6b0f9cebd3a7f5fe2036a2f3dc
沢谷 邦夫 <devel@fathens.org>
2022-02-14 22:23:03 +0900

    Impl to_seed

--- crates/mnemonic/src/calcseed.rs
+++ crates/mnemonic/src/calcseed.rs
@@ -1,7 +1,9 @@
 use super::words::convert_to_num;
 use bytes::Bytes;
+use hmac::Hmac;
 use num_bigint::BigUint;
-use sha2::{Digest, Sha256};
+use pbkdf2::pbkdf2;
+use sha2::{Digest, Sha256, Sha512};
 use std::{
     io::{Error, ErrorKind, Result},
     ops::{BitAnd, Shl},
@@ -47,8 +49,55 @@ fn validate(mnemonic: &Vec<&str>) -> Result<()> {
     }
 }
 
-pub fn to_seed(mnemonic: &Vec<&str>, password: &str) -> Result<Bytes> {
+pub fn to_seed(mnemonic: &Vec<&str>, salt: &str) -> Result<Bytes> {
     validate(mnemonic)?;
-    // TODO Impl
-    Result::Ok(Bytes::default())
+
+    let mut result = vec![0; 64];
+
+    pbkdf2::<Hmac<Sha512>>(
+        mnemonic.join(" ").as_bytes(),
+        ("mnemonic".to_string() + salt).as_bytes(),
+        2048,
+        &mut result,
+    );
+    println!("length = {}", result.len());
+
+    Result::Ok(result.into())
+}
+
+#[cfg(test)]
+mod test {
+    use bytes::Bytes;
+
+    use super::to_seed;
+
+    #[test]
+    fn sample256_seed() {
+        let mnemonic = vec![
+            "menu", "blade", "suit", "police", "family", "snap", "guide", "powder", "protect",
+            "topic", "unfold", "weapon", "remain", "solid", "jacket", "expire", "mind", "certain",
+            "reveal", "cool", "excite", "noise", "stand", "hood",
+        ];
+        let expected_seed = decode_hex("7ec22a3b2a5380aec912b336b89fed7d595cb21e66ba08d99c8012f2bdb1d988d32fd158ee0b8a57f4a4d97e6e34546e1ae7f89ed1ac6dfac2ea312ca93232de").unwrap();
+        let actual_seed = to_seed(&mnemonic, "").unwrap();
+        assert_eq!(expected_seed, actual_seed);
+    }
+
+    #[test]
+    fn sample128_seed() {
+        let mnemonic = vec![
+            "bundle", "elephant", "observe", "exile", "glance", "desk", "above", "flag", "neither",
+            "squeeze", "denial", "day",
+        ];
+        let expected_seed = decode_hex("00e93e7f34b53297cfa9bebffb48bac5e0fe6f79eb88598ea61881d3bde1e50125e56a8bbe6d333be3bf2be8309e2137977c9ac22c3a15ce0212fe26bfbc4b6d").unwrap();
+        let actual_seed = to_seed(&mnemonic, "").unwrap();
+        assert_eq!(expected_seed, actual_seed);
+    }
+
+    fn decode_hex(s: &str) -> Result<Bytes, std::num::ParseIntError> {
+        (0..s.len())
+            .step_by(2)
+            .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
+            .collect()
+    }
 }
